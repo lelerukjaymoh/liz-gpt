c2266d2860761eaf8eb607b18ec2903866e0f4ac
Jay <lelerukjaymoh@gmail.com>
2023-01-21 16:06:48 +0300

    Parse the response which is an SSE due to teh use of stream. The data needs to be parsed so that it can be passed to the google tts

--- src/test/respond.ts
+++ src/test/respond.ts
@@ -2,7 +2,7 @@ import { speechWrapper } from "../speech-processor/client"
 
 const testSpeak = () => {
     try {
-        speechWrapper.speak("hello there, how can i help you")
+        speechWrapper.respond("hello there, how can i help you")
     } catch (error) {
         console.log("Error speaking ", error)
     }
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -6,6 +6,27 @@ class Utils {
         return new Promise((resolve) => setTimeout(resolve, ms));
     }
 
+    parseGPTResponse(response: string): string | undefined {
+        try {
+            let parsedData = ""
+
+            let data = response.split(`"choices": `).slice(1)
+
+            console.log("mid data ", data, response)
+
+
+            data = data.map((item) => item.replace(/\"|\\n/g, ""))
+
+            console.log("mid data ", data)
+
+            data.map((item) => parsedData += item.replace(/\[\{text:/g, "").split(", index")[0])
+
+            return parsedData
+        } catch (error) {
+            console.log("Error parsing the gpt response ", error)
+        }
+
+    }
 }
 
 export const utils = new Utils()
